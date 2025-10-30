8a3965366b071ea7b5b68b15c8348230bc24967a
Stella40k <bernardstella0704@gmail.com>
2025-10-30 10:08:54 -0300

    first commit

---
+++ tarea2/src/App.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import { FirstApp } from "./FirstApp";
+//falta importar el login y el registro
+
---
+++ tarea2/src/FirstApp.jsx
@@ -0,0 +1,11 @@
+export const FirstApp = ()=>{
+    return(
+        <div>
+            <h3>parte de arriba</h3>
+
+            <h1>prueba</h1>
+
+            <h3>parte de abajo</h3>
+        </div>
+    )
+}
